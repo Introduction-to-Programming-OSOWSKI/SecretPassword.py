f65ee157b5c54fcaa18818252f57251ef4e3f5de
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2023-03-27 12:14:57 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 import main;
 import datetime;
 
-year = 2022
-month = 9
-day = 24
+year = 2023
+month = 4
+day = 1
 
 def test_code():
     assert main.password("Knights19") == "ACCESS GRANTED", "password('Knights19') == ACCESS GRANTED failed"
